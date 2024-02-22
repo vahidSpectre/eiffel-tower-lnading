ef04e8847735fe299cb6eea7a4e61490cfaf2d9e
vahidSpectre <vahidsayfollahzadeh@gmail.com>
2024-02-22 15:15:33 +0330

    1.31

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ import "./index.css";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/eiffel-tower-lnading">
         <React.StrictMode>
           <App />
         </React.StrictMode>
